36ce43e74ee7cb5740439ed7fd0fa754f744e51c
stanislav <8268363@gmail.com>
2025-05-01 12:31:28 +0300

    first commit

---
+++ src/main/java/com/surofu/madeinrussia/MadeInRussiaApplication.java
@@ -0,0 +1,13 @@
+package com.surofu.madeinrussia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MadeInRussiaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MadeInRussiaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/surofu/madeinrussia/MadeInRussiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.surofu.madeinrussia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MadeInRussiaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
