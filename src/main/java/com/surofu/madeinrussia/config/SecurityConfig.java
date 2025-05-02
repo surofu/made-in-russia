e6154cf4ad77d277c5b96f2a1d8dbc1463945ca0
stanislav <8268363@gmail.com>
2025-05-02 15:12:34 +0300

    update security config

--- src/main/java/com/surofu/madeinrussia/config/SecurityConfig.java
+++ src/main/java/com/surofu/madeinrussia/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.surofu.madeinrussia.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -17,7 +18,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
-                .cors(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults())
                 .authorizeHttpRequests(auth ->
                         auth.anyRequest().permitAll()
                 );
