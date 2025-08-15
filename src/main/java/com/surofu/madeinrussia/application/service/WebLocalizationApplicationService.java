e83cca7408407f57fbe2eb11124f4ff58047e155
stanislav <8268363@gmail.com>
2025-08-15 15:45:53 +0300

    made web localization caching

--- src/main/java/com/surofu/madeinrussia/application/service/WebLocalizationApplicationService.java
+++ src/main/java/com/surofu/madeinrussia/application/service/WebLocalizationApplicationService.java
@@ -1,5 +1,6 @@
 package com.surofu.madeinrussia.application.service;
 
+import com.surofu.madeinrussia.application.cache.WebLocalizationCacheManager;
 import com.surofu.madeinrussia.application.dto.WebLocalizationDto;
 import com.surofu.madeinrussia.application.service.async.AsyncWebLocalizationService;
 import com.surofu.madeinrussia.core.model.localization.WebLocalization;
@@ -10,37 +11,58 @@ import com.surofu.madeinrussia.core.service.localization.service.GetAllLocalizat
 import com.surofu.madeinrussia.core.service.localization.service.GetLocalizationByLanguageCode;
 import com.surofu.madeinrussia.core.service.localization.service.SaveLocalizationByLanguageCode;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
 import java.util.stream.Collectors;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
-public class WebLocalizationApplicationService implements LocalizationService {
+public class WebLocalizationApplicationService implements LocalizationService, ApplicationRunner {
 
     private final WebLocalizationRepository repository;
     private final AsyncWebLocalizationService asyncService;
+    private final WebLocalizationCacheManager cacheManager;
 
     @Override
     public GetAllLocalizations.Result getAllLocalizations() {
+        Map<String, WebLocalizationDto> cachedAll = cacheManager.getAll();
+
+        if (cachedAll != null && !cachedAll.isEmpty()) {
+            Map<String, Object> content = cachedAll.entrySet().stream().collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+            return GetAllLocalizations.Result.success(new WebLocalizationDto(content));
+        }
+
         List<WebLocalization> localization = repository.getAll();
         Map<String, Object> content = localization.stream()
                 .collect(Collectors.toMap(
                         WebLocalization::getLanguageCode,
                         WebLocalization::getContent
                 ));
+
+        localization.forEach(wl -> cacheManager.setWebLocalization(wl.getLanguageCode(), new WebLocalizationDto(wl.getContent())));
         return GetAllLocalizations.Result.success(new WebLocalizationDto(content));
     }
 
     @Override
     public GetLocalizationByLanguageCode.Result getLocalizationByLanguageCode(GetLocalizationByLanguageCode operation) {
+        WebLocalizationDto cached = cacheManager.getWebLocalization(operation.getLanguageCode());
+
+        if (cached != null) {
+            return GetLocalizationByLanguageCode.Result.success(cached.content());
+        }
+
         Optional<WebLocalization> localization =  repository.getByLanguageCode(operation.getLanguageCode());
 
         if (localization.isEmpty()) {
             return GetLocalizationByLanguageCode.Result.notFound(operation.getLanguageCode());
         }
 
+        cacheManager.setWebLocalization(operation.getLanguageCode(), new WebLocalizationDto(localization.get().getContent()));
         return GetLocalizationByLanguageCode.Result.success(localization.get().getContent());
     }
 
@@ -51,6 +73,7 @@ public class WebLocalizationApplicationService implements LocalizationService {
         localization.setLanguageCode(operation.getLanguageCode());
         localization.setContent(operation.getContent());
         asyncService.save(localization);
+        cacheManager.setWebLocalization(operation.getLanguageCode(), new WebLocalizationDto(operation.getContent()));
         return SaveLocalizationByLanguageCode.Result.success();
     }
 
@@ -58,11 +81,28 @@ public class WebLocalizationApplicationService implements LocalizationService {
     public DeleteLocalizationByLanguageCode.Result deleteLocalization(DeleteLocalizationByLanguageCode operation) {
         Optional<WebLocalization> localization = repository.getByLanguageCode(operation.getLanguageCode());
 
-        if (localization.isPresent()) {
-            asyncService.delete(localization.get());
-            return DeleteLocalizationByLanguageCode.Result.success();
+        if (localization.isEmpty()) {
+            return DeleteLocalizationByLanguageCode.Result.notFound(operation.getLanguageCode());
+
         }
 
-        return DeleteLocalizationByLanguageCode.Result.notFound(operation.getLanguageCode());
+        // TODO: to sync
+        asyncService.delete(localization.get());
+        cacheManager.removeWebLocalization(operation.getLanguageCode());
+        return DeleteLocalizationByLanguageCode.Result.success();
+    }
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        cacheManager.clearAll();
+
+        log.info("Initializing all web localization");
+        getAllLocalizations();
+        log.info("Initializing web localization: en");
+        getLocalizationByLanguageCode(GetLocalizationByLanguageCode.of("en"));
+        log.info("Initializing web localization: ru");
+        getLocalizationByLanguageCode(GetLocalizationByLanguageCode.of("ru"));
+        log.info("Initializing web localization: zh");
+        getLocalizationByLanguageCode(GetLocalizationByLanguageCode.of("zh"));
     }
 }
